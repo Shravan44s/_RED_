3f903f79cc1665a6f87c9575588382dd15e58219
Shravan A Hajare <107493136+Shravan44s@users.noreply.github.com>
2024-03-28 22:56:26 +0530

    Add files via upload

---
+++ crew (2) (1).sql
@@ -0,0 +1,117 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 25, 2023 at 10:03 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crew`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `anon`
+--
+
+CREATE TABLE `anon` (
+  `aid` int(11) NOT NULL,
+  `amsg` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+INSERT INTO `anon` (`aid`, `amsg`) VALUES
+(1, 'shivashabnka'),
+(2, 'hiii'),
+(3, '');
+
+
+
+CREATE TABLE `contact` (
+  `cid` int(3) NOT NULL,
+  `cname` varchar(50) NOT NULL,
+  `cemail` varchar(50) NOT NULL,
+  `cmsg` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+INSERT INTO `contact` (`cid`, `cname`, `cemail`, `cmsg`) VALUES
+(6, 'Nawaz', 'allahuakbar@gmail.com', 'dasdasdas'),
+(7, 'Nawaz1', 'allahuakbar@gmail.com', 'sdhaskhfaskjfhkjashfk'),
+(8, 'navaprettam', 'navaprettam.n214@gmail.com', 'testing testing testing');
+
+
+CREATE TABLE `enrol` (
+  `rid` int(11) NOT NULL,
+  `remail` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+INSERT INTO `enrol` (`rid`, `remail`) VALUES
+(1, 'ss@gmail.com'),
+(2, 'shravn@gmail.com');
+
+
+CREATE TABLE `user` (
+  `uid` int(11) NOT NULL,
+  `uname` varchar(50) DEFAULT NULL,
+  `uphone` int(10) DEFAULT NULL,
+  `uemail` varchar(50) DEFAULT NULL,
+  `upass` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+INSERT INTO `user` (`uid`, `uname`, `uphone`, `uemail`, `upass`) VALUES
+(1, 'shiva', 2147483647, 'ss@gmail.com', '8439eb993385d58771334b4d8149b893'),
+(2, 'shiva', 2147483647, 'ss1@gmail.com', '8439eb993385d58771334b4d8149b893'),
+(3, 'shravan', 2147483647, 'shravan@gmail.com', '8439eb993385d58771334b4d8149b893'),
+(4, 'shravan', 2147483647, 'shravanhajare@gmail.com', 'e10adc3949ba59abbe56e057f20f883e'),
+(5, 'navaprettam', 2147483647, 'navaprettam.n@gmail.com', '202cb962ac59075b964b07152d234b70'),
+(6, 'navaprettam', 2147483647, 'navaprettam.n214@gmail.com', '202cb962ac59075b964b07152d234b70');
+
+ALTER TABLE `anon`
+  ADD PRIMARY KEY (`aid`);
+
+
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`cid`);
+
+ALTER TABLE `enrol`
+  ADD PRIMARY KEY (`rid`);
+
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`uid`);
+
+
+ALTER TABLE `anon`
+  MODIFY `aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+ALTER TABLE `contact`
+  MODIFY `cid` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+
+ALTER TABLE `enrol`
+  MODIFY `rid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+
+ALTER TABLE `user`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
